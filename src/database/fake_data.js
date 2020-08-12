efcf657c6d501b0eeb7b08faaca0245724413ca4
AbadePetterson <abadepetterson@gmail.com>
2020-08-12 07:25:33 -0300

    Primeira versão projeto

---
+++ src/database/fake_data.js
@@ -0,0 +1,38 @@
+//Dados
+const proffys = [
+    {
+        name: "Diego Fernandes", 
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "9999987878", 
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Quimíca", 
+        cost:"20", 
+        weekday:[0], 
+        time_from:[720], 
+        time_to:[1220]
+    },
+
+    {
+        name: "Abade Petterson", 
+        avatar:"https://avatars2.githubusercontent.com/u/26881234?s=460&u=752dcfbf92c29ccb2e73938c7eefb579d75b9467&v=4",
+        whatsapp: "9999987878", 
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Quimíca", 
+        cost:"20", 
+        weekday:[1], 
+        time_from:[720], 
+        time_to:[1220]
+    },
+
+    {
+        name: "Ed Dev", 
+        avatar:"https://avatars2.githubusercontent.com/u/31110235?s=460&u=23b23bec508e19607f7ca66353d31eaab9907638&v=4",
+        whatsapp: "9999987878", 
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Quimíca", 
+        cost:"20", 
+        weekday:[3], 
+        time_from:[720], 
+        time_to:[1220]
+    }
+]
